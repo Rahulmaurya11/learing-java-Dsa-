8e42a751a2c5010f03db043c52469b7228e281e6
rahulmaurya11 <rahulmaurya1500@gmail.com>
2022-08-20 12:21:19 +0530

    DSA git learning strings

---
+++ PalidromeString.java
@@ -0,0 +1,19 @@
+public class PalidromeString {
+    public static void main(String[] args) {
+        String str = "abzba";
+        System.out.println(ispalindrome(str));
+        }
+        static boolean ispalindrome(String str){
+str=str.toLowerCase();
+
+for(int i=0;i<str.length()/2;i++){
+char start = str.charAt(i);
+char end = str.charAt(str.length()-1-i);
+if(start!=end){
+    return false;
+}
+}
+    return true;
+    }
+    }
+
